045e0784d4c7e8400c96f7875255ea2c8a1be2e3
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-29 15:40:47 +0100

    creato oggetto

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà.
+var studente ={
+  'nome': 'Stefano',
+  'cognome': 'Riccio',
+  'eta': 31
+}
+for (var key in studente) {
+console.log(studente[key]);
+}
+
+
+// - Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+// - Dare la possibilità all’utente attraverso 3 prompt di
+// aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
