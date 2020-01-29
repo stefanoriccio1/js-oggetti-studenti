ed7348d21de458f020ff74172b9d800f395c921e
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-29 16:12:47 +0100

    up

--- js/script.js
+++ js/script.js
@@ -10,5 +10,35 @@ console.log(studente[key]);
 
 
 // - Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
+
+var studenti = [
+  {
+    'nome': 'Stefano',
+    'cognome': 'Riccio',
+    'eta': 31
+  },
+  {
+    'nome': 'Marco',
+    'cognome': 'Riccio',
+    'eta': 30
+  },
+  {
+    'nome': 'Giovanni',
+    'cognome': 'Riccio',
+    'eta': 32
+  }
+]
+console.log(studenti[1]['eta']);
+
+
+  for (var key in studenti[i]) {
+   if (key == 'nome'){
+     console.log('nome');
+   }
+   else if(key == 'cognome'){
+     console.log('cognome');
+   }
+  }
+
 // - Dare la possibilità all’utente attraverso 3 prompt di
 // aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
