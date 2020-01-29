c7a8efa4b13ffc195f5b3a655c30b34d628ebcf9
Stefano Riccio <riccio.stefano0@gmail.com>
2020-01-29 17:06:17 +0100

    up

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 // Creare un oggetto che descriva uno studente con le seguenti proprietà: nome, cognome e età. Stampare a schermo attraverso il for in tutte le proprietà.
-var studente ={
-  'nome': 'Stefano',
-  'cognome': 'Riccio',
-  'eta': 31
-}
-for (var key in studente) {
-console.log(studente[key]);
-}
+
+// var studente ={
+//   'nome': 'Stefano',
+//   'cognome': 'Riccio',
+//   'eta': 31
+// }
+// for (var key in studente) {
+// console.log(studente[key]);
+// }
 
 
 // - Creare un array di oggetti di studenti. Ciclare su tutti gli studenti e stampare per ognuno nome e cognome
@@ -28,13 +29,15 @@ var studenti = [
     'eta': 32
   }
 ]
-console.log(studenti[1]['eta']);
-
-
+// console.log(studenti[1]['eta']);
   for (var key in studenti) {
    console.log(studenti[key].nome + ' ' + studenti[key].cognome);
+}
 
-   // for (var i = 0; i < studenti[key].length; i++) {
-   //   console.log('ciao');
-   // }
+}
+// Dare la possibilità all’utente attraverso 3 prompt di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+var nuovoStudente = {
+  'nome': '',
+  'cognome': '',
+  'eta': ''
 }
